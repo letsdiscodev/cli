8f5b4300d5a97acd488d7ebfbb0fe365a9e897e0
gregsadetsky <lepetitg@gmail.com>
2024-05-12 22:41:30 -0400

    .

--- release.sh
+++ release.sh
@@ -1,4 +1,7 @@
+# exit on error
 set -e
+# print every command being run
+set -x
 
 # fail if git status shows changes not staged for commit
 if [[ `git status --porcelain` ]]; then
@@ -18,6 +21,21 @@ if [ -z "${AWS_SECRET_ACCESS_KEY}" ]; then
   exit 1
 fi
 
+# get the latest release pushed to the repo
+latest_release=$(gh release list --limit 1 --json tagName --jq '.[0].tagName')
+
+# get the version by cat'ing package.json into jq and extracting the version
+version=$(cat package.json | jq -r '.version')
+
+# check if the latest release is the same as the version in package.json
+if [ "$latest_release" = "$version" ]; then
+  echo "The latest release is the same as the version in package.json. Please update the version in package.json before running this script"
+  exit 1
+fi
+
+
+# ---
+
 rm -rf dist
 rm -rf tmp
 
@@ -25,7 +43,12 @@ npm ci
 
 npm run build
 
-oclif pack tarballs --parallel --no-xz
+oclif pack tarballs --no-xz
 
-# TODO upload tarballs
+oclif upload tarballs --no-xz
+
 # TODO promote tarballs -- extract hash somehow
+# get the hash from calling python and passing -c "...some script"
+hash=$(python3 -c "from pathlib import Path; print(str(list(Path('./dist').glob('disco-v0.5.5*'))[0]).split('-')[2])")
+
+oclif promote --sha $hash --version $version --no-xz
