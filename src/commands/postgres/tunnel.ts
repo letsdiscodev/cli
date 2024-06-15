e46fe71fb11b1e1c50caa8be8291cbfee06933cf
Antoine Leclair <antoineleclair@gmail.com>
2024-06-15 17:41:39 -0400

    Got something working

--- src/commands/postgres/tunnel.ts
+++ src/commands/postgres/tunnel.ts
@@ -1,6 +1,7 @@
 import {Command, Flags} from '@oclif/core'
 import {getDisco} from '../../config.js'
 import {request} from '../../auth-request.js'
+import {createTunnel} from 'tunnel-ssh';
 
 export default class PostgresTunnel extends Command {
   static description = 'create a temporary tunnel to access Postgres through localhost'
@@ -9,18 +10,32 @@ export default class PostgresTunnel extends Command {
 
   static flags = {
     disco: Flags.string({required: false}),
-    instance: Flags.string({required: true}),
+    project: Flags.string({required: true}),
+    'env-var': Flags.string({required: false}),
   }
 
   public async run(): Promise<void> {
     const {flags} = await this.parse(PostgresTunnel)
     const discoConfig = getDisco(flags.disco || null)
+    
+    const dbInfoUrl = `https://${discoConfig.host}/api/projects/postgres-addon/cgi/endpoints/tunnels`;
+    const dbInfoResponse = await request({
+      method: 'POST',
+      url: dbInfoUrl,
+      body: {
+        project: flags.project,
+        envVar: flags['env-var'],
+      },
+      discoConfig,
+      expectedStatuses: [200],
+    })
+    const dbInfoRespBody = (await dbInfoResponse.json()) as {dbInfo: {instance: string, database: string, user: string, password: string}}
+    const {dbInfo} = dbInfoRespBody;
     const url = `https://${discoConfig.host}/api/tunnels`
     const reqBody = {
-        project: `postgres-instance-${flags.instance}`,
+        project: `postgres-instance-${dbInfo.instance}`,
         service: 'postgres',
     };
-
     const res = await request({
       method: 'POST',
       url,
@@ -29,8 +44,42 @@ export default class PostgresTunnel extends Command {
       expectedStatuses: [201],
     })
     const respBody = (await res.json()) as {tunnel: {host: string, password: string, port: number}}
-    this.log(`host: ${respBody.tunnel.host}`)
-    this.log(`password: ${respBody.tunnel.password}`)
-    this.log(`port: ${respBody.tunnel.port}`)
+    const connString = `postgresql://${dbInfo.user}:${dbInfo.password}@localhost/${dbInfo.database}`
+    
+
+    const sshOptions = {
+      host: discoConfig.host,
+      port: respBody.tunnel.port,
+      username: 'root',
+      password: respBody.tunnel.password
+    };
+
+  
+    const forwardOptions = {
+        dstAddr: respBody.tunnel.host,
+        dstPort: 5432
+    }
+
+    const tunnelOptions = {
+        autoClose: true
+    }
+    
+    const serverOptions = {
+        host: 'localhost',
+        port: 5432
+    }
+
+    await createTunnel(tunnelOptions, serverOptions, sshOptions, forwardOptions);
+
+    
+    
+    
+    
+    // this.log(`host: ${respBody.tunnel.host}`)
+    // this.log(`password: ${respBody.tunnel.password}`)
+    // this.log(`port: ${respBody.tunnel.port}`)
+
+    this.log("Tunnel created. Connection string:")
+    this.log(connString)
   }
 }
