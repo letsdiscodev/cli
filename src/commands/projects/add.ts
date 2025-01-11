35de37ecf60bb177122c8966575c5f2ddcf9c433
gregsadetsky <lepetitg@gmail.com>
2025-01-10 19:51:29 -0500

    check if user has access to repo when running projects:add, fixes #44. also fixes #49

--- src/commands/projects/add.ts
+++ src/commands/projects/add.ts
@@ -14,13 +14,18 @@ export default class ProjectsAdd extends Command {
       description: 'domain name where the app will be served, e.g. www.example.com',
     }),
     github: Flags.string({
-      required: false,
+      required: true,
       description:
         'full name of the Github repository, including user or organization and repository name, e.g. myuser/myproject',
     }),
     branch: Flags.string({
       required: false,
       description: 'the branch of the repository to use',
+    }),
+    deployPublicRepo: Flags.boolean({
+      required: false,
+      description:
+        'deploy a public repository without checking for GitHub access. Note that "git push" to the repo will not trigger a new deployment',
     }),
     disco: Flags.string({required: false, description: 'server to use'}),
   }
@@ -33,6 +38,14 @@ export default class ProjectsAdd extends Command {
     }
 
     const discoConfig = getDisco(flags.disco || null)
+
+    if (!flags.deployPublicRepo && !(await isGithubRepoAuthorized(discoConfig, flags.github))) {
+      this.error(`disco does not have access to this GitHub repository.
+
+Either set up GitHub access by running "disco github:apps:add"
+or edit your GitHub repo permissions by running "disco github:apps:manage <your github username>".`)
+    }
+
     const url = `https://${discoConfig.host}/api/projects`
 
     const body = {
@@ -59,4 +72,15 @@ export default class ProjectsAdd extends Command {
       })
     }
   }
+}
+
+async function isGithubRepoAuthorized(discoConfig: any, repoBeingChecked: string) {
+  // check if the user has access to the github repo
+  const url = `https://${discoConfig.host}/api/github-app-repos`
+
+  const res = await request({method: 'GET', url, discoConfig})
+  const data = (await res.json()) as any
+
+  const authorizedRepos = data.repos.map((r: any) => r.fullName)
+  return authorizedRepos.includes(repoBeingChecked)
 }
